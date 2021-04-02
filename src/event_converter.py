7c063e0b1ef11f6c1c89cc94ed69bbd1b1a41417
Francesco Areoluci <francesco.areoluci@magentalab.it>
2021-04-02 18:50:10 +0200

    Added polarity encoder

--- src/event_converter.py
+++ src/event_converter.py
@@ -13,7 +13,52 @@ sys.path.insert(0, '../prophesee-automotive-dataset-toolbox/')
 from src.io.psee_loader import PSEELoader
 
 
-def encode_video(N, width, height, video, encoder, delta=1000):
+def encode_video_polarity(width, height, video, delta=20000):
+    '''
+        @brief: Encode video in a sequence of frames using
+                Polarity encoding
+    '''
+
+    # Each encoded frame will have a start/end timestamp (ms) in order
+    # to associate bounding boxes later.
+    # Note: If videos are longer than 1 minutes, 16 bits per ts are not sufficient.
+    data_type = np.dtype([('startTs', np.uint16), 
+                            ('endTs', np.uint16), 
+                            ('frame', np.float64, (height, width))])
+    
+    samplePerVideo = math.ceil(video.total_time() / delta)
+    polarity_array = np.zeros(samplePerVideo, dtype=data_type)
+
+    i = 0
+    startTimestamp = 0
+    endTimestamp = 0
+
+    pbar = tqdm(total=samplePerVideo, file=sys.stdout)
+    while not video.done:
+        events = video.load_delta_t(delta)
+        f = np.full(video.get_size(), 0.5)
+        for e in events:
+            # Evaluate polarity of an event 
+            # for a certain pixel
+            if e['p'] == 1:
+                f[e['y'], e['x']] = 1
+            else:
+                f[e['y'], e['x']] = 0
+
+        endTimestamp += delta / 1000
+        polarity_array[i]['startTs'] = startTimestamp
+        polarity_array[i]['endTs'] = endTimestamp
+        polarity_array[i]['frame'] = f
+        startTimestamp += delta / 1000
+        i += 1
+        
+        pbar.update(1)
+
+    pbar.close()
+    return polarity_array
+
+
+def encode_video_tbe(N, width, height, video, encoder, delta=1000):
     '''
         @brief: Encode an event video in a sequence of frame
                 using the Temporal Binary Representation
@@ -35,30 +80,31 @@ def encode_video(N, width, height, video, encoder, delta=1000):
     startTimestamp = 0
     endTimestamp = 0
 
-    with tqdm(total = samplePerVideo, file = sys.stdout) as pbar:
-        while not video.done:
-            i = (i + 1) % N
-            # Load next 1ms events from the video
-            events = video.load_delta_t(delta)
-            f = np.zeros(video.get_size())
-            #f = np.zeros((width, height))
-            for e in events:
-                # Evaluate presence/absence of event for
-                # a certain pixel
-                f[e['y'], e['x']] = 1
+    pbar = tqdm(total = samplePerVideo, file = sys.stdout)
+    while not video.done:
+        i = (i + 1) % N
+        # Load next 1ms events from the video
+        events = video.load_delta_t(delta)
+        f = np.zeros(video.get_size())
+        #f = np.zeros((width, height))
+        for e in events:
+            # Evaluate presence/absence of event for
+            # a certain pixel
+            f[e['y'], e['x']] = 1
 
-            accumulation_mat[i, ...] = f
+        accumulation_mat[i, ...] = f
 
-            if i == N - 1:
-                endTimestamp += N
-                tbe = encoder.encode(accumulation_mat)
-                tbe_array[j]['startTs'] = startTimestamp
-                tbe_array[j]['endTs'] = endTimestamp
-                tbe_array[j]['frame'] = tbe
-                j += 1
-                startTimestamp += N
-                pbar.update(1)
-
+        if i == N - 1:
+            endTimestamp += N
+            tbe = encoder.encode(accumulation_mat)
+            tbe_array[j]['startTs'] = startTimestamp
+            tbe_array[j]['endTs'] = endTimestamp
+            tbe_array[j]['frame'] = tbe
+            j += 1
+            startTimestamp += N
+            pbar.update(1)
+    
+    pbar.close()
     return tbe_array
 
 
@@ -183,7 +229,7 @@ def setupDirectories(root_dir):
     valid_file_path = custom_path + '/valid.txt'
     test_file_path = custom_path + '/test.txt'
     completed_file_path = data_path + "/completed_videos"
-    evaluated_tbe_path = data_path + "/evaluated_tbe"
+    evaluated_enc_path = data_path + "/evaluated_enc"
 
     if not os.path.isdir(data_path):
         os.mkdir(data_path)
@@ -199,8 +245,8 @@ def setupDirectories(root_dir):
     if not os.path.isdir(labels_path):
         os.mkdir(labels_path)
 
-    if not os.path.isdir(evaluated_tbe_path):
-        os.mkdir(evaluated_tbe_path)
+    if not os.path.isdir(evaluated_enc_path):
+        os.mkdir(evaluated_enc_path)
 
     ## Setup classes
     f = open(classes_file_path, "w")
@@ -232,7 +278,7 @@ def setupDirectories(root_dir):
             "test_file": test_file_path,
             "list": list_path,
             "completed": completed_file_path,
-            "tbe": evaluated_tbe_path
+            "enc": evaluated_enc_path
             }
 
 
@@ -268,14 +314,14 @@ def setupArgParser():
     '''
 
     parser = argparse.ArgumentParser(description='Convert events to frames and associates bboxes')
-    parser.add_argument('--use_stored_tbe', '-l', action='count', default=0,
-                        help='use_stored_tbe: instead of evaluates TBR, uses pre-evaluated TBR array. Default: false')
-    parser.add_argument('--save_tbe', '-s', action='count', default=0,
-                        help='save_tbe: save the intermediate Temporal Binary Represented frame array. Default: false')
+    parser.add_argument('--use_stored_enc', '-l', action='count', default=0,
+                        help='use_stored_enc: instead of evaluates TBR or other encodings, uses pre-evaluated encoded array. Default: false')
+    parser.add_argument('--save_enc', '-s', action='count', default=0,
+                        help='save_enc: save the intermediate TBR or other encodings frame array. Default: false')
     parser.add_argument('--show_video', '-v', action='count', default=0,
                         help='show_video: show video with evaluated TBR frames and their bboxes during processing. Default: false')
-    parser.add_argument('--accumulate', '-n', type=int, nargs=1,
-                        help='accumulator: set the number of events to be accumulated. Default: 16')
+    parser.add_argument('--tbr_bits', '-n', type=int, nargs=1,
+                        help='tbr_bits: set the number of bits for Temporal Binary Representation. Default: 16')
     parser.add_argument('--src_video', '-t', type=str, nargs=1,
                         help='src_video: path to event videos')
     parser.add_argument('--dest_path', '-d', type=str, nargs=1,
@@ -286,21 +332,24 @@ def setupArgParser():
                         help='save_bb_img: save frame with bboxes to path')
     parser.add_argument('--accumulation_time', '-a', type=int, nargs=1,
                         help='accumulation_time: set the quantization time of events (microseconds). Default: 1000')
+    parser.add_argument('--encoder', '-c', type=str, nargs=1,
+                        help='encoder: set the encoder: <tbe | polarity>. Default: tbe')
 
     return parser.parse_args()
 
 
 ## Parsing arguments
 args = setupArgParser()
-save_tbe = True if args.save_tbe > 0 else False
-use_stored_tbe = True if args.use_stored_tbe > 0 else False
+save_encoding = True if args.save_enc > 0 else False
+use_stored_encoding = True if args.use_stored_enc > 0 else False
 show_video = True if args.show_video > 0 else False
-accumulate_requested = True if args.accumulate != None else False
+tbr_bits_requested = True if args.tbr_bits != None else False
 src_video_requested = True if args.src_video != None else False
 dest_path_requested = True if args.dest_path != None else False
 event_type_requested = True if args.event_type != None else False
 save_path_requested = True if args.save_bb_img != None else False
 accumulation_time_requested = True if args.accumulation_time != None else False
+encoder_type_requested = True if args.encoder != None else False
 
 dest_root_folder = '..'
 if dest_path_requested:
@@ -331,10 +380,18 @@ if event_type_requested:
 ## Iterate through videos in video_dir to get list 
 video_names = getEventList(video_dir)
 
+## Encoder
+requested_encoder = 'tbe'
+if encoder_type_requested:
+    if args.encoder[0] == 'tbe' or args.encoder[0] == 'polarity':
+        requested_encoder = args.encoder[0]
+    else:
+        print("Invalid encoder requested. Using default encoder (tbe)")
+
 ## Number of events to be accumulated
 N = 16
-if accumulate_requested and args.accumulate[0] > 0:
-    N = args.accumulate[0]
+if tbr_bits_requested and args.tbr_bits[0] > 0:
+    N = args.tbr_bits[0]
 
 ## Accumulation time (microseconds)
 delta_t = 1000
@@ -344,10 +401,13 @@ if accumulation_time_requested and args.accumulation_time[0] > 0:
 ## Print some info
 print("Event to frame converter")
 print("===============================")
-print("Requested TBR array saving: " + str(save_tbe))
-print("Requested saved TBR array loading: " + str(use_stored_tbe))
+print("Encoder: " + requested_encoder)
+print("Requested TBR array saving: " + str(save_encoding))
+print("Requested saved TBR array loading: " + str(use_stored_encoding))
 print("Requested video show during processing: " + str(show_video))
-print("Accumulating {:d} events".format(N))
+if requested_encoder == 'tbe':
+    print("Accumulating {:d} events".format(N))
+print("Accumulation time: " + str(delta_t))
 print("Source event path: " + video_dir)
 print("Destination path: " + dest_root_folder + '/data')
 print("Event data type: " + data_type)
@@ -380,20 +440,26 @@ for video_name in video_names:
     height = video.get_size()[0]
     encoder = TemporalBinaryEncoding(N, width, height)
 
-    if not use_stored_tbe:
+    if not use_stored_encoding:
         ## Convert event video to a Temporal Binary Encoded frames array
-        tbe_array = encode_video(N, width, height, video, encoder, delta_t)
-        if save_tbe:
-            np.save(dir_paths["tbe"] + video_name + "_tbe.npy", tbe_array)
+        if requested_encoder == 'tbe':
+            encoded_array = encode_video_tbe(N, width, height, video, encoder, delta_t)
+        elif requested_encoder == 'polarity' and accumulation_time_requested:
+            encoded_array = encode_video_polarity(width, height, video, delta_t)
+        else:
+            encoded_array = encode_video_polarity(width, height, video)
+
+        if save_encoding:
+            np.save(dir_paths["enc"] + video_name + "_enc.npy", encoded_array)
     else:
-        ## Use the pre-evaluated TBE array
-        tbe_array = np.load(dir_paths["tbe"] + video_name + "_tbe.npy")
+        ## Use the pre-evaluated encoded (tbe or else) array
+        encoded_array = np.load(dir_paths["enc"] + video_name + "_enc.npy")
 
     ## Iterate through video frames
     img_count = 0
     bbox_count = 0
     print("Saving TBE frames and bounding boxes...")
-    for f in tbe_array:
+    for f in encoded_array:
         bboxes = get_frame_BB(f, arr)
 
         filename = video_name + str("_" + str(f["startTs"]))
@@ -438,7 +504,7 @@ for video_name in video_names:
             show_image(f['frame'], bboxes)
             plt.pause(0.05)
 
-    print("Saved {:d} TBR frames in path: {:s}".format(img_count, dir_paths["images"]))
+    print("Saved {:d} encoded frames in path: {:s}".format(img_count, dir_paths["images"]))
     print("Saved {:d} bounding boxes annotations in path: {:s}".format(bbox_count, dir_paths["labels"]))
 
     completed_file = open(dir_paths['completed'], "a")
